602433b10b91aabbb483c75e07e24674d386309e
akirademoss <akirademoss@gmail.com>
2019-02-14 14:51:08 -0600

    adding project1

---
+++ project1/include.h
@@ -0,0 +1,8 @@
+#include <time.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <string.h>
+#include<readline/readline.h> 
+#include<readline/history.h> 
