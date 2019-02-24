92073e7a6d113d27e312eafbf83e514762e741af
= <=>
2019-02-24 11:21:53 -0600

    added functionality for executable binaries searchable by path or on the path

--- project1/308sh.c
+++ project1/308sh.c
@@ -2,6 +2,9 @@
 #include <limits.h>
 #define MAXLINE  50
 
+//boolean type
+typedef enum bool { false, true };
+
 /**
  * Code to parse user input into an array of strings
  */
@@ -11,7 +14,6 @@ char** parse(void){
 	char line[50];
 	char* linepass;
 	char* p;
-
 	//gets the input from user, replace '\n' with '\0', then tokenize input
 	fgets(line, MAXLINE, stdin);
     if(p=strchr(line, '\n')){
@@ -19,15 +21,12 @@ char** parse(void){
 	}
 	linepass = strtok(line, " ");
 	args[0] = linepass;
-
-
 	//gets arguments preceeding inital command and stores them into an array
 	int i = 1;
 	while(linepass != NULL){
 		linepass = strtok(NULL, " ");
 		if(linepass != NULL){
 			args[i] = linepass;
-			printf("%s\n", args[i]);
 		}
 		i++;
 	}
@@ -52,31 +51,36 @@ char* getShellName(int argc, char* argv[]){
 /**
  * Builtin Command functionalities
  */
-void builtIn(char** args){
+enum bool builtIn(char** args){
 	char cwd[PATH_MAX];
+	//builtIn commands logic
 	if (strcmp(args[0], "exit") == 0){ exit(0);}
-	if (strcmp(args[0], "pid") == 0){ printf("PID: %d\n", getpid());}
-	if (strcmp(args[0], "ppid") == 0){ printf("PPID: %d\n", getppid());}
-	if (strcmp(args[0], "get") == 0){ printf("%s\n", getenv(args[1]));}
-	if (strcmp(args[0], "cd") == 0){ if(args[1] != NULL){ chdir(args[1]);}}
-	if (strcmp(args[0], "pwd") == 0){ 
-		if (getcwd(cwd, sizeof(cwd)) != NULL) {
-      		printf("%s\n", cwd);
-   		} else {
-       		perror("getcwd() error");
-		}
+	else if (strcmp(args[0], "pid") == 0){ printf("PID: %d\n", getpid());}
+	else if (strcmp(args[0], "ppid") == 0){ printf("PPID: %d\n", getppid());}
+	else if (strcmp(args[0], "get") == 0){ printf("%s\n", getenv(args[1]));}
+	else if (strcmp(args[0], "cd") == 0){ if(args[1] != NULL){ chdir(args[1]);}}
+	else if (strcmp(args[0], "pwd") == 0){ 
+		if (getcwd(cwd, sizeof(cwd)) != NULL) {printf("%s\n", cwd);} else { perror("getcwd() error");}}
+	else if (strcmp(args[0], "set") == 0){ 
+		if(args[1] != NULL){if(args[2] == NULL){unsetenv(args[1]);}else{ setenv(args[1], args[2], 1);}}}
+	else{
+		return false;
 	}
-	if (strcmp(args[0], "set") == 0){ 
-		if(args[1] != NULL){
-			if(args[2] == NULL){
-				unsetenv(args[1]);
-			}
-			else{
-				setenv(args[1], args[2], 1);
-			}
-		}
+	return true;
+}
+
+void executable(char** args){
+	printf("begin executable: %s\n", args[0]);
+	int failed = 0;
+	char* command = malloc((MAXLINE + 1) * sizeof(char));
+	strcpy(command, args[0]);
+	failed = execvp(command, args);
+	if(failed <0){
+		printf("Cannot exec command '%s': No such file or path variable exists\n", command);
 	}
 }
+
+
 
 /**
  * Primary functional logic
@@ -92,6 +96,10 @@ int main(int argc, char *argv[]){
 
 	printf("%s", shellName);
 	cmdArgs = parse();
-	builtIn(cmdArgs);
+	if(builtIn(cmdArgs) < 1){
+		printf("main before executable: %s\n", cmdArgs[0]);
+		executable(cmdArgs);
+	}
+
 	}
 }
